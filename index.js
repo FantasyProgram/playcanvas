977cf5922e7831da933e7454b48b7c0bc445c8f3
FantasyProgram <99851461+FantasyProgram@users.noreply.github.com>
2024-12-13 04:02:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function getQueryParameter(name) {
     return urlParams.get(name);  // Returns the value of the parameter or null if not found
 }
 
+
+window.onload = function(){
+    var ageObj =   document.getElementById('age');
+
+    let value = getQueryParameter("age") ? : "nothing" ;
+    ageObj.innerText = value;
+};
+
+
