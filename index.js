eb8401d1d2b289166cfb6fb84dca7c5ea0d60a1b
FantasyProgram <99851461+FantasyProgram@users.noreply.github.com>
2024-12-13 03:52:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+function getQueryParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);  // Returns the value of the parameter or null if not found
+}
 
